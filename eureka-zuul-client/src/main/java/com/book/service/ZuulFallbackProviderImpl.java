f7b505e385d34935ae3f23a5e8764c58df4b89fe
luoxian <15290810931@163.com>
2018-11-22 22:36:33 +0800

    zuul 网关配合熔断器使用[需要实现ZuulFallbackProvider接口]，* 代表所有的微服务

---
+++ eureka-zuul-client/src/main/java/com/book/service/ZuulFallbackProviderImpl.java
@@ -0,0 +1,62 @@
+package com.book.service;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @Author: 一点点
+ * @Date: 2018/11/22 22:05
+ * @Version 1.0
+ */
+@Component
+public class ZuulFallbackProviderImpl implements ZuulFallbackProvider{
+   @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("oooops!error,i'm the fallbak".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
